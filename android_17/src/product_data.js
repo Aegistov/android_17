1f6638b5fd723e8148f20b2db982c0ab2a641ef8
JMonk54 <J_Monk_living@yahoo.com>
2019-12-25 12:13:46 -0500

    Change product_data

--- android_17/src/product_data.js
+++ android_17/src/product_data.js
@@ -1,28 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-export class ProductData extends React.Component {
-    render() {
-        const productData = [
-            {
-                id: 1,
-                product_sid: '5MJBQJ',
-                name: 'apple',
-                price: '1.49'
-            },
-            {
-                id: 3,
-                product_sid: '58ZIX6',
-                name: 'strawberries',
-                price: '4.98'
-            }
-        ];
-        return (
-            <div>
-                {productData}
-            </div>
-        )
+
+const ProductData = [
+    {
+        id: 1,
+        product_sid: '5MJBQJ',
+        name: 'apple',
+        price: '1.49'
+    },
+    {
+        id: 3,
+        product_sid: '58ZIX6',
+        name: 'strawberries',
+        price: '4.98'
     }
-}
+];
 
 export default ProductData;
 
